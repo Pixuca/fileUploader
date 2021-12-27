feca33447a359fc074fdb13f97c46531c624d04b
Pixuca <marcelo.macedo330@hotmail.com>
2021-12-27 16:14:26 -0300

    alteracoes feitas no script e nas paginas web

--- testFlask.py
+++ testFlask.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, flash, request, redirect
+from flask import Flask, flash, request, redirect, send_from_directory
 from flask.helpers import make_response
 from flask.templating import render_template
 from flask_restful import Resource, Api
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'C:/Users/marce/Documents/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'rar', 'exe', 'iso', 'mp4', 'hevc', 'mp3', 'wav', 'avi'}
+ALLOWED_EXTENSIONS = {'txt', 'GIF', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'rar', 'exe', 'iso', 'mp4', 'hevc', 'mp3', 'wav', 'avi'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,7 +16,6 @@ api = Api(app)
 # PÁGINA PRINCIPAL
 class AppointmentController(Resource):
     def get(self):
-        headers = {'Content-Type': 'text/html'}
         return make_response(render_template('index.html'))
 
     def post(self):
@@ -33,18 +32,31 @@ class AppointmentController(Resource):
             else:
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return f'O arquivo {file.filename} foi upado com sucesso!'
+                return make_response(render_template('upload.html'))
 
 # PÁGINA COM A LISTA DOS ARQUIVOS UPADOS
 class UplodadedFiles(Resource):
     def get(self):
-        headers = {'Content-Type': 'text/html'}
         path = 'C:/Users/marce/Documents/uploads'
-        lista = os.listdir(path)
-        return make_response(render_template('list.html', lista=lista))
+        array = os.listdir(path)
+        extArray = []
+        for item in array:
+            ext = item.split('.')
+            extArray.append(ext[-1])
+        arraySize = len(array)
+
+        return make_response(render_template('list.html', array=array, extArray=extArray, arraySize=arraySize))
+
+
+# DOWNLOAD DE ARQUIVO
+class DownloadFiles(Resource):
+    def get(self, name):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+
 
 api.add_resource(AppointmentController, '/')
 api.add_resource(UplodadedFiles, '/list')
+api.add_resource(DownloadFiles, '/list/<name>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
